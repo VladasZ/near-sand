61d41888785ca9abe89de37902d2920429f65055
Vladas <146100@gmail.com>
2023-09-11 22:16:44 +0300

    a

--- contract/deploy.sh
+++ contract/deploy.sh
@@ -10,4 +10,4 @@ fi
 echo ">> Deploying contract"
 
 # https://docs.near.org/tools/near-cli#near-dev-deploy
-near dev-deploy --wasmFile ./target/wasm32-unknown-unknown/release/hello_near.wasm
+near deploy --accountId vladasz.testnet --wasmFile ./target/wasm32-unknown-unknown/release/hello_near.wasm
